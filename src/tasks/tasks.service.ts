9f3f8b2d185d9b9cee2ab4dbbf12f586547b787b
noobj <jingfu99@gmail.com>
2021-05-10 11:50:14 +0800

    fix: add missing await

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -57,7 +57,7 @@ export class TasksService {
             };
         });
 
-        const result = this.summariesService.upsert(fetchedData);
+        const result = await this.summariesService.upsert(fetchedData);
 
         console.log(result);
     }
