4ef69ac7d3d941103ae337e91c15af7caf6a06d6
noobj <jingfu99@gmail.com>
2021-05-17 17:42:18 +0800

    test: first try testing

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,22 +1,40 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthService } from './app/auth/auth.service';
+type MockType<T> = {
+    [P in keyof T]?: jest.Mock<{}>;
+};
 
 describe('AppController', () => {
     let appController: AppController;
+    let authService: AuthService;
 
     beforeEach(async () => {
+        const serviceMockFactory: () => MockType<AuthService> = jest.fn(() => ({
+            login: jest.fn((abc) => '123'),
+        }));
+
         const app: TestingModule = await Test.createTestingModule({
+            providers: [
+                // Provide your mock instead of the actual repository
+                { provide: AuthService, useFactory: serviceMockFactory },
+            ],
             controllers: [AppController],
-            providers: [AppService],
         }).compile();
 
+        authService = app.get(AuthService);
         appController = app.get<AppController>(AppController);
     });
 
     describe('root', () => {
         it('should return "Hello World!"', () => {
-            expect(appController.getHello()).toBe('Hello World!');
+            jest.spyOn(authService, 'login').mockImplementationOnce((a) =>
+                Promise.resolve({ access_token: '123' })
+            );
+            appController.login('123')
+            .then((re) => {
+                expect(re.access_token).toEqual('123');
+            });
         });
     });
 });
