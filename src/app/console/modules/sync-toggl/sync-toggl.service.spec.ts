60371209d85abee0ccbb3cd0e94a8835db844999
jjj <jingfu99@gmail.com>
2021-05-23 14:52:23 +0800

    test: sync-toggl.service test

--- src/app/console/modules/sync-toggl/sync-toggl.service.spec.ts
+++ src/app/console/modules/sync-toggl/sync-toggl.service.spec.ts
@@ -4,6 +4,7 @@ import configuration from 'src/config/configuration';
 import { ConfigModule } from '@nestjs/config';
 import { SummariesService } from 'src/app/modules/summaries';
 import { TogglClient } from './TogglClient';
+jest.mock('./TogglClient.ts');
 
 describe('SyncTogglService', () => {
     let service: SyncTogglService;
@@ -12,6 +13,29 @@ describe('SyncTogglService', () => {
         getProjectIdByName: jest.fn(() => Promise.resolve(157099012)),
         upsert: jest.fn(() => Promise.resolve('done')),
     };
+
+    TogglClient.mockImplementation(() => {
+        return {
+            getWorkSpaceId: () => 123,
+            getDetails: () => ({
+                data: [
+                    {
+                        start: '2021-01-10T11:00:51+08:00',
+                        dur: 1000,
+                    },
+                    {
+                        start: '2021-01-10T11:00:51+08:00',
+                        dur: 1000,
+                    },
+                    {
+                        start: '2021-01-11T11:00:51+08:00',
+                        dur: 1000,
+                    },
+                ],
+                total_count: 3,
+            }),
+        };
+    });
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
@@ -33,5 +57,9 @@ describe('SyncTogglService', () => {
         const spyLog = jest.spyOn(console, 'log').mockImplementation();
         await service.run(['3']);
         expect(spyLog).toBeCalledWith('done');
+        expect(mockSummariesService.upsert).toBeCalledWith([
+            { date: '2021-01-10', project: 157099012, duration: 2000 },
+            { date: '2021-01-11', project: 157099012, duration: 1000 },
+        ]);
     });
 });
