c0d7f61226ab452b8d02bc74d08e9b586d610df3
jjj <jingfu99@gmail.com>
2021-05-23 17:42:00 +0800

    test: fix mock TogglClient error

--- src/app/console/modules/sync-toggl/sync-toggl.service.spec.ts
+++ src/app/console/modules/sync-toggl/sync-toggl.service.spec.ts
@@ -3,8 +3,32 @@ import { SyncTogglService } from './sync-toggl.service';
 import configuration from 'src/config/configuration';
 import { ConfigModule } from '@nestjs/config';
 import { SummariesService } from 'src/app/modules/summaries';
-import { TogglClient } from './TogglClient';
-jest.mock('./TogglClient.ts');
+jest.mock('./TogglClient', () => {
+    return {
+        TogglClient: jest.fn().mockImplementation(() => {
+            return {
+                getWorkSpaceId: () => 123,
+                getDetails: () => ({
+                    data: [
+                        {
+                            start: '2021-01-10T11:00:51+08:00',
+                            dur: 1000,
+                        },
+                        {
+                            start: '2021-01-10T11:00:51+08:00',
+                            dur: 1000,
+                        },
+                        {
+                            start: '2021-01-11T11:00:51+08:00',
+                            dur: 1000,
+                        },
+                    ],
+                    total_count: 3,
+                }),
+            };
+        }),
+    };
+});
 
 describe('SyncTogglService', () => {
     let service: SyncTogglService;
@@ -13,29 +37,6 @@ describe('SyncTogglService', () => {
         getProjectIdByName: jest.fn(() => Promise.resolve(157099012)),
         upsert: jest.fn(() => Promise.resolve('done')),
     };
-
-    TogglClient.mockImplementation(() => {
-        return {
-            getWorkSpaceId: () => 123,
-            getDetails: () => ({
-                data: [
-                    {
-                        start: '2021-01-10T11:00:51+08:00',
-                        dur: 1000,
-                    },
-                    {
-                        start: '2021-01-10T11:00:51+08:00',
-                        dur: 1000,
-                    },
-                    {
-                        start: '2021-01-11T11:00:51+08:00',
-                        dur: 1000,
-                    },
-                ],
-                total_count: 3,
-            }),
-        };
-    });
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
