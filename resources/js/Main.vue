3e0cc88add9eb32e16fbc435987e17436fcda4f3
Bandy <jaky0089@outlook.com>
2021-05-29 15:41:22 +0800

    refactor: add Main for global layout #11

---
+++ resources/js/Main.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="main" class="flex flex-col items-center dark:text-white dark:bg-gray-800">
+    <Menu />
+    <Dates />
+  </div>
+</template>
+
+<script>
+import Dates from './components/Dates'
+import Menu from './components/Menu'
+
+export default {
+  name: 'Main',
+  components: {
+    Menu,
+    Dates
+  },
+  data () {
+    return {
+    }
+  },
+  methods: {
+  }
+}
+</script>
