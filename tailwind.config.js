ede4e8f516b7bf7c931900f013106df8db50b72c
Bandy <jaky0089@outlook.com>
2021-05-31 15:42:47 +0800

    build: use vue-cli native way to copy assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     purge: [
-        './resources/*.html',
+        './public/*.html',
         './resources/**/*.js',
         './resources/**/*.vue'
     ],
--- vue.config.js
+++ vue.config.js
@@ -1,38 +1,22 @@
-const path = require('path');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-
 module.exports = {
     devServer: {
         proxy: {
             '/': {
                 target: 'http://localhost:3000',
-                changeOrigin: true,
+                changeOrigin: true
             },
         },
-        public: '0.0.0.0:3001',
+        public: '0.0.0.0:3001'
     },
     outputDir: 'dist/public',
     pages: {
         index: {
             entry: 'resources/js/app.js',
-            template: 'resources/index.html',
+            template: 'public/index.html'
         },
         login: {
             entry: 'resources/js/login.js',
-            template: 'resources/login.html',
-        },
-    },
-    configureWebpack: {
-        plugins: [
-            new CopyWebpackPlugin({
-                patterns: [
-                    {
-                        from: path.resolve(__dirname, 'resources/img'),
-                        to: path.resolve(__dirname, 'dist/public/img'),
-                        toType: 'dir',
-                    },
-                ],
-            }),
-        ],
-    },
-};
+            template: 'public/login.html'
+        }
+    }
+}
