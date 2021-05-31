08148f17d85ffd57d8dd2c985d61c84ac7b8f71e
Bandy <jaky0089@outlook.com>
2021-05-31 16:36:58 +0800

    refactor(Login): script without html tag

--- resources/js/login.js
+++ resources/js/login.js
@@ -1,1 +1,21 @@
 import '/resources/css/app.css'
+
+document.querySelector('.login-form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    const url = '/auth/login'
+    const account = document.querySelector('form.login-form > [name=account]')
+    const password = document.querySelector('form.login-form > [name=password]')
+    fetch(url, {
+        method: 'POST',
+        credentials: 'include',
+        body: new URLSearchParams({
+            'account': account.value,
+            'password': password.value
+        }),
+    }).then((res) => {
+        if (res.status != 201) throw new Error()
+        window.location.href = '/'
+    }).catch(() => {
+        alert('login failed')
+    })
+})
