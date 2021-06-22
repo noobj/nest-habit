275e5f2ab1a532c8b4c69b30e01774a0dcd7ab06
jjj <jingfu99@gmail.com>
2021-06-22 15:01:11 +0800

    refactor: replace async with rxjs in summaries

--- src/app/modules/summaries/summaries.controller.ts
+++ src/app/modules/summaries/summaries.controller.ts
@@ -13,11 +13,14 @@ import {
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { IsDateString } from 'class-validator';
+import { forkJoin, from, of } from 'rxjs';
+import { map, mergeMap, tap } from 'rxjs/operators';
 
 import { IBasicService } from './interfaces/basic.service';
 import { Interfaces } from './constants';
 import { ProjectService } from './projects.service';
 import { HttpExceptionFilter } from 'src/common/exception-filters/http-exception.filter';
+import { DailySummary } from './entities';
 
 class DateRange {
     @IsDateString()
@@ -38,67 +41,84 @@ export class SummariesController {
     @UseGuards(AuthGuard('jwt'))
     @Post('project')
     @UseFilters(new HttpExceptionFilter())
-    async setCurrentProjectByName(@Request() req, @Body('project_name') projectName) {
-        await this.projectService.setCurrentProject(req.user, projectName);
+    setCurrentProjectByName(@Request() req, @Body('project_name') projectName) {
+        return from(this.projectService.setCurrentProject(req.user, projectName)).pipe(
+            map(() => 'done')
+        );
     }
 
     @UseGuards(AuthGuard('jwt'))
     @Get('projects')
-    async getProjectNameByUser(@Request() req) {
-        const curretProject = await this.projectService.getProjectByUser(req.user);
-        const allProjects = await this.projectService
-            .getAllProjects(req.user)
-            .then((res) => {
+    getProjectNameByUser(@Request() req) {
+        return forkJoin({
+            curretProject: this.projectService.getProjectByUser(req.user),
+            allProjects: this.projectService.getAllProjects(req.user).then((res) => {
                 return res.data.map((entry) => entry.name);
-            });
+            }),
+        }).pipe(
+            map(({ curretProject, allProjects }) => {
+                const result =
+                    curretProject == undefined
+                        ? {
+                              allProjects: allProjects,
+                              currentProject: null,
+                          }
+                        : {
+                              allProjects: allProjects,
+                              currentProject: curretProject,
+                          };
 
-        const result =
-            curretProject == undefined
-                ? {
-                      allProjects: allProjects,
-                      currentProject: null,
-                  }
-                : {
-                      allProjects: allProjects,
-                      currentProject: curretProject,
-                  };
-
-        return {
-            statusCode: HttpStatus.OK,
-            data: result,
-        };
+                return {
+                    statusCode: HttpStatus.OK,
+                    data: result,
+                };
+            })
+        );
     }
 
     @UseGuards(AuthGuard('jwt'))
     @Get('summaries')
     async showAll(@Query(new ValidationPipe()) dateRange: DateRange, @Request() req) {
-        const rawData = await this.summariesService.getRawDailySummaries(
-            dateRange.start_date,
-            dateRange.end_date,
-            req.user
+        return from(
+            this.summariesService.getRawDailySummaries(
+                dateRange.start_date,
+                dateRange.end_date,
+                req.user
+            )
+        ).pipe(
+            mergeMap((rawData: DailySummary[]) => {
+                if (rawData.length === 0) {
+                    return of({
+                        statusCode: HttpStatus.NO_CONTENT,
+                        data: 'No Data',
+                    });
+                } else {
+                    return forkJoin({
+                        summries: this.summariesService.processTheRawSummaries(rawData),
+                        longestRecord: of(
+                            this.summariesService.getLongestDayRecord(rawData)
+                        ),
+                        totalYear: of(this.summariesService.getTotalDuration(rawData)),
+                        totalThisMonth: of(
+                            this.summariesService.getTotalThisMonth(rawData)
+                        ),
+                    }).pipe(
+                        map(({ summries, longestRecord, totalYear, totalThisMonth }) => {
+                            const result = {
+                                summaries: summries,
+                                longest_record: longestRecord,
+                                total_last_year: totalYear,
+                                total_this_month: totalThisMonth,
+                            };
+
+                            return {
+                                statusCode: HttpStatus.OK,
+                                data: result,
+                            };
+                        })
+                    );
+                }
+            })
         );
-
-        if (rawData.length === 0) {
-            return {
-                statusCode: HttpStatus.NO_CONTENT,
-                data: 'No Data',
-            };
-        }
-        const summries = await this.summariesService.processTheRawSummaries(rawData);
-        const longestRecord = this.summariesService.getLongestDayRecord(rawData);
-        const totalYear = this.summariesService.getTotalDuration(rawData);
-        const totalThisMonth = this.summariesService.getTotalThisMonth(rawData);
-
-        const result = {
-            summaries: summries,
-            longest_record: longestRecord,
-            total_last_year: totalYear,
-            total_this_month: totalThisMonth,
-        };
-
-        return {
-            statusCode: HttpStatus.OK,
-            data: result,
-        };
     }
 }
--- src/app/modules/summaries/summaries.gateway.ts
+++ src/app/modules/summaries/summaries.gateway.ts
@@ -11,8 +11,8 @@ import { ProjectService } from './projects.service';
 import { SummariesService } from './summaries.service';
 import { endOfToday, subYears, subDays, format } from 'date-fns';
 import { ModuleRef } from '@nestjs/core';
-import { Observable, from, zip, of } from 'rxjs';
-import { map, mergeMap, filter } from 'rxjs/operators';
+import { Observable, from, forkJoin } from 'rxjs';
+import { map, mergeMap } from 'rxjs/operators';
 
 @WebSocketGateway(3002)
 export class SummariesGateway implements OnModuleInit {
@@ -32,15 +32,13 @@ export class SummariesGateway implements OnModuleInit {
 
     @UseGuards(AuthGuard('jwt'))
     @SubscribeMessage('sync')
-    // async onEvent(socket, data): Promise<WsResponse<string>> {
     onEvent(socket, data): Observable<WsResponse<string>> {
-        // Rxjs approach
         return from(
             this.projectService.setCurrentProject(socket.user, data.projectName)
         ).pipe(
             mergeMap(() => {
                 const tmpEnd = endOfToday();
-                const tmpStart = subDays(tmpEnd, 1);
+                const tmpStart = subYears(tmpEnd, 1);
                 const endDate = format(tmpEnd, 'yyyy-MM-dd');
                 const startDate = format(subDays(tmpStart, 0), 'yyyy-MM-dd');
                 return this.summariesService.getRawDailySummaries(
@@ -50,15 +48,15 @@ export class SummariesGateway implements OnModuleInit {
                 );
             }),
             mergeMap((rawData) => {
-                return zip(
-                    this.summariesService.processTheRawSummaries(rawData),
-                    this.projectService.getProjectByUser(socket.user)
-                ).pipe(
-                    map(([summaries, currentProject]) => {
+                return forkJoin({
+                    summaries: this.summariesService.processTheRawSummaries(rawData),
+                    currentProject: this.projectService.getProjectByUser(socket.user),
+                }).pipe(
+                    map(({ summaries, currentProject }) => {
                         if (rawData.length === 0) {
                             return {
                                 current_project: currentProject,
-                            }
+                            };
                         }
 
                         const longestRecord =
@@ -79,45 +77,5 @@ export class SummariesGateway implements OnModuleInit {
             }),
             map((val) => ({ event: 'sync', data: JSON.stringify(val) }))
         );
-
-        /** async/awake approach
-            await this.projectService.setCurrentProject(socket.user, data.projectName);
-            const currentProject = await this.projectService.getProjectByUser(socket.user);
-
-            const tmpEnd = endOfToday();
-            const tmpStart = subYears(tmpEnd, 1);
-            const endDate = format(tmpEnd, 'yyyy-MM-dd');
-            const startDate = format(subDays(tmpStart, 7), 'yyyy-MM-dd');
-            const rawData = await this.summariesService.getRawDailySummaries(
-                startDate,
-                endDate,
-                socket.user
-            );
-
-            if (rawData.length === 0) {
-                return {
-                    event: 'sync',
-                    data: JSON.stringify({ current_project: currentProject }),
-                };
-            }
-
-            const summries = await this.summariesService.processTheRawSummaries(rawData);
-            const longestRecord = this.summariesService.getLongestDayRecord(rawData);
-            const totalYear = this.summariesService.getTotalDuration(rawData);
-            const totalThisMonth = this.summariesService.getTotalThisMonth(rawData);
-
-            const result = {
-                summaries: summries,
-                longest_record: longestRecord,
-                total_last_year: totalYear,
-                total_this_month: totalThisMonth,
-                current_project: currentProject,
-            };
-
-            return {
-                event: 'sync',
-                data: JSON.stringify(result),
-            };
-         **/
     }
 }
