49242de759a33e8f62dfb8c421eca2beff325c09
noobj <jingfu99@gmail.com>
2021-07-19 11:15:01 +0800

    fix: tailwind will purge VueTailwindCssPicker

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
+const path = require('path');
+
 module.exports = {
     purge: [
         './public/*.html',
         './resources/**/*.js',
-        './resources/**/*.vue'
+        './resources/**/*.vue',
+        path.resolve(__dirname, './node_modules/vue-tailwind-picker/**/*.js')
     ],
     darkMode: 'media',
     plugins: [
