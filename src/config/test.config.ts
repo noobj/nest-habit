ca4f0ed87136105d7032459de6ba1676311934ba
noobj <jingfu99@gmail.com>
2021-09-13 17:20:54 +0800

    test: fix test module couldnt find Quote entity #38

--- src/config/test.config.ts
+++ src/config/test.config.ts
@@ -1,6 +1,7 @@
 import { User } from 'src/app/modules/users/users.entity';
 import { DailySummary } from 'src/app/modules/summaries/entities/daily_summary.entity';
 import { Project } from 'src/app/modules/summaries/entities/project.entity';
+import { Quote } from 'src/app/modules/quote/quote.entity';
 
 export default () => ({
     port: parseInt(process.env.PORT, 10) || 3000,
@@ -11,7 +12,7 @@ export default () => ({
         database: 'test',
         username: process.env.DB_USER || 'linuxj', // fetch the main.yml setting for github actions
         password: process.env.DB_PASSWORD || '1234', // fetch the main.yml setting for github actions
-        entities: [User, DailySummary, Project],
+        entities: [User, DailySummary, Project, Quote],
         synchronize: true,
         logging: false,
     },
@@ -19,7 +20,7 @@ export default () => ({
         secret: process.env.JWT_SECRET || 'secret',
         expiration_time: process.env.JWT_EXPIRATION_TIME || 3600,
         refresh_secret: process.env.JWT_REFRESH_TOKEN_SECRET || 'secret',
-        refresh_expiration_time: process.env.JWT_REFRESH_TOKEN_EXPIRATION_TIME || 360000,
+        refresh_expiration_time: process.env.JWT_REFRESH_TOKEN_EXPIRATION_TIME || 360000
     },
     session: {
         secret: process.env.SESSION_SECRET || 'secret',
