dfc137659591ddbf89453f870faeec4b17059171
noobj <jingfu99@gmail.com>
2021-12-01 18:02:01 +0800

    build: fix migration-test Unknown database error

--- src/database/migrations/1638352161847-drop_notify_id_column_in_users.ts
+++ src/database/migrations/1638352161847-drop_notify_id_column_in_users.ts
@@ -2,7 +2,7 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class dropNotifyIdColumnInUsers1638352161847 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE habbit.users DROP COLUMN notify_id;`);
+        await queryRunner.query(`ALTER TABLE users DROP COLUMN notify_id;`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
